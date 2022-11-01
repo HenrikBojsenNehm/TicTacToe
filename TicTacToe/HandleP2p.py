1c96845e74846b945da1e7d3f77aa39c4fe450b9
HenrikBojsenNehm <henriknehmdk@gmail.com>
2022-11-01 15:12:23 +0100

    client testing

--- TicTacToe/HandleP2p.py
+++ TicTacToe/HandleP2p.py
@@ -15,6 +15,12 @@ class p2pHost:
 class p2pClient:
     print('Client')
 
+    clientPort = 50001
+
+    clientName = socket.gethostname()
+    clientIp = socket.gethostbyname(clientName)
+    print(f'Your Computer IP Address is: {clientIp}\nUse port: {clientPort}')
+
 
     print('Connect to rendezvous server')
 
@@ -28,9 +34,13 @@ class p2pClient:
     # connect to rendezvous
     print('connecting to rendezvous server')
 
+    # punch hole
+    print('punching hole')
+
+
     sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    sock.bind(('0.0.0.0', 50001))
-    sock.sendto(b'0', gameHost)
+    sock.bind(('0.0.0.0', clientPort))
+    sock.sendto(b'ready', (hostIp, hostPort))
 
     while True:
         data = sock.recv(1024).decode()
@@ -38,18 +48,6 @@ class p2pClient:
         if data.strip() == 'ready':
             print('checked in with server, waiting')
             break
-
-    data = sock.recv(1024).decode()
-    hostIp, clientPort, hostPort = data.split(' ')
-    clientPort = int(clientPort)
-    hostPort = int(hostPort)
-
-    # punch hole
-    print('punching hole')
-
-    sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    sock.bind(('0.0.0.0', clientPort))
-    sock.sendto(b'0', (hostIp, hostPort))
 
     print('ready to exchange messages\n')
 
@@ -72,3 +70,44 @@ class p2pClient:
     while True:
         msg = input('> ')
         sock.sendto(msg.encode(), (hostIp, hostPort))
+
+    # while True:
+    #     data = sock.recv(1024).decode()
+
+    #     if data.strip() == 'ready':
+    #         print('checked in with server, waiting')
+    #         break
+
+    # data = sock.recv(1024).decode()
+    # hostIp, clientPort, hostPort = data.split(' ')
+    # clientPort = int(clientPort)
+    # hostPort = int(hostPort)
+
+    # # punch hole
+    # print('punching hole')
+
+    # sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    # sock.bind(('0.0.0.0', clientPort))
+    # sock.sendto(b'0', (hostIp, hostPort))
+
+    # print('ready to exchange messages\n')
+
+    # # listen for messages
+    # def listen(clientPort):
+    #     sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    #     sock.bind(('0.0.0.0', clientPort))
+
+    #     while True:
+    #         data = sock.recv(1024)
+    #         print('\rpeer: {}\n> '.format(data.decode()), end='')
+
+    # listener = threading.Thread(target=listen, args=(clientPort,), daemon=True);
+    # listener.start()
+
+    # # send messages
+    # sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    # sock.bind(('0.0.0.0', hostPort))
+
+    # while True:
+    #     msg = input('> ')
+    #     sock.sendto(msg.encode(), (hostIp, hostPort))
